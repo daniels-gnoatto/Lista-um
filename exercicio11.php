0e7c65d9f8eb7f20c6ce4bcfdc7c8977852883ed
Daniels_gnoatto <daniel_gnoatto@estudante.sesisenai.org.br>
2025-07-17 09:45:02 -0300

    Finalizado o exercicio 11

---
+++ exercicio11.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form method="POST" action="">
+        <label for="text">Veja se a palavra é Palindromo:</label>
+        <input type="text" id="text" name="text" required>
+        <button type="submit" name="verificar_text">Verificar</button>
+    </form>
+
+    <?php
+
+    if (isset($_POST['text'])) {
+        $string = $_POST['text'];
+        echo "$string";
+        echo "<br>";
+        echo strrev($string);
+        $reverse = strrev($string);
+    }
+
+    if ($reverse == $string){
+        echo "<br>";
+        echo "É palindromo";
+    }else{
+        echo "<br>";
+        echo "Não é palindromo";
+    }
+
+
+
+
+    ?>
+
+
+</body>
+</html>
